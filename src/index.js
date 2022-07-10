fd1ca6b7eecbee05ce337f4d181f5716e220038c
unknown <btrn86@gmail.com>
2022-07-10 03:32:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const stack = [];
+  const errors = [];
+  const opposite = bracketsConfig.reduce((a, b) => {
+    a[b[0]] = b[1];
+    return a;
+  }, {});
+  const openBrackets = Object.keys(opposite);
+  const closeBrackets = Object.values(opposite);
+  console.log(str);
+
+  [...str].forEach(char => {
+    if (openBrackets.includes(char) && closeBrackets.includes(char)) {
+      if (char === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else {
+      if (openBrackets.includes(char)) {
+        stack.push(char)
+      } else {
+        if (closeBrackets.includes(char)) {
+          char === opposite[stack[stack.length - 1]] ? stack.pop() : errors.push(char);
+        } else {
+          errors.push(char);
+        }
+      }
+    }
+  })
+  return !stack.length && !errors.length;
 }
